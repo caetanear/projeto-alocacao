e8635d1a4d9894755252729e01e3e374aeaed62c
Thiago Rocha <30945155+mariothiago@users.noreply.github.com>
2023-01-30 13:54:03 -0300

    AAAAAAAAAAAAAAAA trying

--- js/form.js
+++ js/form.js
@@ -5,7 +5,18 @@ botaoAdicionar.addEventListener("click", function(event) {
     var form = document.querySelector("#form-adiciona");
 
     var alocacao = obtemAlocacaoDoFormulario(form);
-
+    
+    // Método para enviar alocacao para backend via FETCH. LEMBRETE: ALTERAR URL PARA URL DO ENDPOINT
+    fetch('https://localhost:5000/api/endpoint', {
+      method: "POST",
+      body: JSON.stringify(alocacao),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json));
+    .catch(err => console.log(err));
+    // FIM DO MÉTODO
+    
     var alocacaoTr = montaTr(alocacao);
 
     var tabela = document.querySelector("#tabela-alocacoes");
