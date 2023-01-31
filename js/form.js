f68165057120c8af9b66b6af8bdd0d65271210e4
J. M <juliasoriju@gmail.com>
2023-01-31 18:21:20 -0300

    blah

--- js/form.js
+++ js/form.js
@@ -1,20 +1,20 @@
 var botaoAdicionar = document.querySelector("#adicionar");
-botaoAdicionar.addEventListener("click", function(event) {
+botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
 
     var alocacao = obtemAlocacaoDoFormulario(form);
-    
+
     // Método para enviar alocacao para backend via FETCH. LEMBRETE: ALTERAR URL PARA URL DO ENDPOINT
     fetch('https://localhost:5000/api/endpoint', {
-      method: "POST",
-      body: JSON.stringify(alocacao),
-      headers: {"Content-type": "application/json; charset=UTF-8"}
+        method: "POST",
+        body: JSON.stringify(alocacao),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
     })
-    .then(response => response.json()) 
-    .then(json => console.log(json))
-    .catch(err => console.log(err));
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
     // FIM DO MÉTODO
     var alocacaoTr = montaTr(alocacao);
 
@@ -47,6 +47,8 @@ function montaTr(alocacao) {
     alocacaoTr.appendChild(montaTd(alocacao.sala, "info-sala"));
     alocacaoTr.appendChild(montaTd(alocacao.data, "info-data"));
     alocacaoTr.appendChild(montaTd(alocacao.turno, "info-turno"));
+    alocacaoTr.appendChild(montaTd(alocacao.turno, "info-acoes"));
+
 
     return alocacaoTr;
 }
