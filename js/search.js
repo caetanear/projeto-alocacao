2ce0aa79343752ddcaac81bb82f1beeaa1808cf4
J. M <juliasoriju@gmail.com>
2023-02-07 11:16:31 -0300

    barra buscar

---
+++ js/search.js
@@ -0,0 +1,9 @@
+var botaoEnter = document.querySelector("#barra-enter");
+
+var input = document.querySelector("#buscar-palavra");
+
+botaoEnter.addEventListener('click', function(event)){
+    var buscar = input.value.search,
+
+    return buscar.value;
+}
