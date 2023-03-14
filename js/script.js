4730416e133d01b7dae97d8b3b3d9772157d0f20
J. M <juliasoriju@gmail.com>
2023-03-14 09:17:46 -0300

    senhor

--- js/script.js
+++ js/script.js
@@ -126,3 +126,132 @@ class Produto {
 }
 
 var produto = new Produto();
+
+/* class Alocacao {
+
+    constructor(){
+        this.id = 1;
+        this.arrayAlocacao = [];
+        this.editId = null;
+    }
+
+    salvar(){
+        let nome = this.lerDados();
+
+        if(this.validaCampos(nome)){
+            if(this.editId == null){
+                this.adicionar(nome);
+            } else{
+                this.atualizar(this.editId, nome);
+            }
+        }
+
+        this.listaTabela();
+        this.cancelar();
+    }
+
+    listaTabela(){
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
+
+        for(let i = 0; i < this.arrayAlocacao.length; i++){
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_nome = tr.insertCell();
+            let td_valor = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            td_id.innerText = this.arrayAlocacao[i].id;
+            td_nome.innerText = this.arrayAlocacao[i].nomeProf;
+            td_valor.innerText = this.arrayAlocacao[i].curso;
+            
+            td_id.classList.add('center');
+            td_acoes.classList.add('center');
+
+            let imgEdit = document.createElement('img');
+            imgEdit.src = 'img/edit.png';
+            imgEdit.setAttribute("onclick", "nome.preparaEdicao("+ JSON.stringify(this.arrayAlocacao[i]) +")");
+
+            let imgDelete = document.createElement('img');
+            imgDelete.src = 'img/delete.png';
+            imgDelete.setAttribute("onclick", "nome.deletar("+ this.arrayAlocacao[i].id +")");
+
+            td_acoes.appendChild(imgEdit);
+            td_acoes.appendChild(imgDelete);
+
+            console.log(this.arrayAlocacao);
+        }
+    }
+
+    adicionar(nome){
+        this.arrayAlocacao.push(nome);
+        this.id++;
+    }
+
+    atualizar(id, nome){
+        for(let i = 0; i < this.arrayAlocacao.length; i++){
+            if(this.arrayAlocacao[i].id == id){
+                this.arrayAlocacao[i].nomeProf = nome.nomeProf;
+                this.arrayAlocacao[i].curso = nome.curso;
+            }
+        }
+    }
+
+    preparaEdicao(dados){
+        this.editId = dados.id;
+
+        document.getElementById('nome').value = dados.nomeProf;
+        document.getElementById('curso').value = dados.curso;
+
+        document.getElementById('btn-salvar').innerText = 'Atualizar';
+    }
+
+    lerDados(){
+        let nome = {}
+            nome.id = this.id;
+            nome.nomeProf = document.getElementById('nome').value;
+            nome.curso = document.getElementById('curso').value;
+        
+            return nome;
+        }
+
+    validaCampos(nome){
+        let mensagem = '';
+        if(nome.nomeProf == ''){
+            mensagem += 'Informe o campo nome \n';
+        }
+        if(nome.curso == ''){
+            mensagem += 'Informe o campo curso \n';
+        }
+        if(mensagem != ''){
+            return false;
+        }
+        return true;
+    }
+
+    cancelar(){
+        document.getElementById('nome').value = '';
+        document.getElementById('curso').value = '';
+
+        document.getElementById('btn-salvar').innerText = 'Salvar';
+        this.editId = null;
+    }
+
+    deletar(id){
+
+        if(confirm('Deseja deletar o campo ' + id + '?')){
+
+        let tbody = document.getElementById('tbody');
+
+        for(let i = 0; i < this.arrayAlocacao.length; i++){
+            if(this.arrayAlocacao[i].id == id){
+                this.arrayAlocacao.splice(i, 1);
+                tbody.deleteRow(i);
+            }
+        }
+    }
+    }
+}
+
+var nome = new Alocacao(); */
--- js/search.js
+++
@@ -1,7 +0,0 @@
-var botaoEnter = document.querySelector('table');
-
-var palavraBuscar = input.value;
-
-botaoEnter.addEventListener('click', function(event)){
-    var buscar = palavraBuscar.search('');
-}
